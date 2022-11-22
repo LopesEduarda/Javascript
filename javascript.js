5b91c2be6e2a0e1ca75deb8e32623f96607afce3
Maria Eduarda <eduardaloopes2011@hotmail.com>
2022-11-22 12:28:03 -0300

    explicando o resultado

--- javascript.js
+++ javascript.js
@@ -5,4 +5,9 @@ function ganhoPorHora(salario, horasTrabalhadasNoMes) {
     return salarioHora;
 }
 
-console.log(ganhoPorHora(3000, 200));
+console.log(ganhoPorHora(3000, 176));
+/*
+O valor por hora do salário seria: 17.045454545454547. Mas, como estamos falando de um valor monetário, esse número deveria ter apenas duas casas decimais. Isto é, o que queremos é arredondar esse resultado para reduzir as casas decimais.
+
+Como estamos utilizando o JavaScript, já existe um método da classe Math que arredonda os números, a Math.round().
+*/
