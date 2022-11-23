555902700ab05308e94248ebbedd298ba764d0e0
Maria Eduarda <eduardaloopes2011@hotmail.com>
2022-11-23 15:34:07 -0300

    expressao de funcao

--- funcoes.js
+++ funcoes.js
@@ -24,3 +24,37 @@ console.log(soma());
 /*
  tem uma vantagem muito importante de quebrarmos o nosso programa em funções relativamente pequenas, que fica mais fácil para nós lermos. Nós sabemos o que, exatamente, aquele trecho de código está fazendo. Conseguimos reaproveitar o trecho do código e fica bem mais fácil darmos manutenção.
 */
+
+// Os parâmetros servem pra função saber o que deve receber para funcionar
+// Boas práticas para funções é passar poucos argumentos. Se necessário, criar várias funções com poucos argumentos pois fica mais fácil, inclusive, de dar manutenção.
+
+function cumprimentaPessoa(pessoa) {
+    console.log(`oi, ${pessoa}!`)
+}
+
+cumprimentaPessoa('Helena')
+
+
+function cumprimentar() {
+    return 'Oi gente!'
+}
+
+function cumprimentaPessoa(nomePessoa) {
+    console.log(`${cumprimentar()} Meu nome é ${nomePessoa}`)
+}
+
+cumprimentaPessoa('Paula') // “Oi gente! Meu nome é Paula”
+
+
+
+// Outra forma de escrever função em javascript: Expressão de função
+
+const somando = function(num1, num2) {
+    return num1 + num2;
+};
+
+console.log(somando(2, 2));
+// Aqui criamos uma variável e atribuimos nossa função à essa variável. É importante ser const pra nao corrermos risco de colocar outro valor dentro dela. Um jeito mais curto de escrever a função também.
+
+// A função declarativa pode ser chamada em qualquer parte do código, inclusive antes de sua criação
+// A função expressão de função deve ser chamada somente nas linhas abaixo de sua criação
