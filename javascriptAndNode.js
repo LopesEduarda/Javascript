053bfa5419e6c56f26463aa27aa9bbb55467444d
Maria Eduarda <eduardaloopes2011@hotmail.com>
2022-11-23 13:40:59 -0300

    javascript e node js

--- javascriptAndNode.js
+++ javascriptAndNode.js
@@ -1,8 +1,36 @@
 /*
  Agora que já vimos um pouco dos fundamentos do JavaScript, como declarar variáveis, o que são tipos, o que são operadores, vamos voltar um pouco para entender um pouco mais por trás de como funciona a linguagem e o que são algumas ferramentas que já viemos utilizando.
 
+ A primeira característica do JavaScript, que pode ser diferente de algumas outras linguagens que você já deu uma olhada quando estava decidindo para qual caminho ia, por exemplo, é que o JavaScript é uma linguagem que chamamos de tipagem dinâmica. 
+*/
 
+// Além disso, podemos reatribuir valores para as variáveis, dessa forma:
+let minhaVar = 500;
+minhaVar = "Eduarda";
+// Podemos reatribuir o valor de uma variável para um valor com uma tipagem diferente da anterior.
 
+// Do Javascript, podemos dizer que é uma linguagem multiparadigma: quer dizer que conseguimos resolver o mesmo problema de várias formas diferentes.
 
- 
+/* Por que se chama JavaScript? O nome mesmo da linguagem JavaScript é ECMAScript.
+ECMA é o nome da fundação que cuida dos padrões e dos standarts para a linguagem, então, o que vai ser desenvolvido, as features que vão ser desenvolvidas, o que a linguagem precisa melhorar, atualizações, etc.
+
+ES6 ou ES2015 é a versão do JavaScript que saiu em 2015. Ela acabou sendo uma versão muito marcante, porque a partir desse ano começamos a ter versões anuais, com algumas atualizações anuais.
+*/
+
+/*
+Vamos primeiro falar que tem dois tipos de linguagens, as interpretadas e as compiladas.
+
+Na linguagem interpretada, nós vamos escrever o nosso código e o nosso código vai ser executado. Então, nós vamos ter um programa por trás, que vai ler a linha do nosso código e executar o que está escrito nessa linha na hora em que estivermos rodando.
+
+ Já linguagens compiladas, nosso código vai passar primeiro por um programa chamado compilador, que vai transformar o nosso código em código de máquina, linguagem de máquina. É um tipo especial de linguagem que o computador entende.
+*/
+
+/*
+O que é o tal do NodeJS? Que ouvimos também “Você programa em Node?”. Na verdade, ninguém programa em Node, não é? Porque o Node nada mais é que o JavaScript, o Node não é uma linguagem.
+
+ O JavaScript, como é uma linguagem interpretada, ele precisa de um interpretador. Nós podemos usar um navegador como interpretador ou podemos usar o Node como interpretador. Se usamos o Node, nós, geralmente, vamos usar o JavaScript no backend, ou seja, no nosso lado do servidor. Para ele executar os códigos que queremos no servidor.
+
+ Então, podemos dizer que quando o JavaScript surgiu, o Node não existia. O Node é uma ferramenta que foi desenvolvida em 2010 (eu acho), bem depois do JavaScript já existir, para nós conseguirmos usar o JavaScript fora do interpretador padrão dele, que é o navegador (que é o que está aparecendo na tela).
+
+ Então, o Node é um ambiente para gerar códigos JavaScript fora do navegador.
 */
