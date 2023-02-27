7a65abeb3786af2a2299c1a7c87641f8e15de269
Maria Eduarda <eduardaloopes2011@hotmail.com>
2023-02-27 19:45:34 -0300

    scrolling through objects

---
+++ Objetos/percorrendo-objetos.js
@@ -0,0 +1,4 @@
+/*
+ vamos aprender uma maneira de iterar dentro de um objeto. Vamos supor que temos o nosso objeto cliente que possui diversos campos, e desejamos montar um relatório usando essas propriedades (campos). Neste cenário, assumimos que não sabemos todos os campos.
+*/
+
