264f63344cf42187dab8b2b62da764b9a627d639
Maria Eduarda <eduardaloopes2011@hotmail.com>
2023-02-27 20:31:17 -0300

    JSON

---
+++ JSON/formato-json.js
@@ -0,0 +1,23 @@
+/*
+JSON é uma sigla para JavaScript Object Notation, ou "Notação de Objeto JavaScript" em português. Ou seja, é um formato que permite a escrita de objetos do JavaScript.
+*/
+
+/*
+Notamos algumas diferenças na forma de escrita desse mesmo objeto no arquivo .js e no arquivo .json.
+
+Analisando detalhadamente o formato JSON, percebemos algumas particularidades que desconhecemos:
+
+Não existem variáveis
+Chaves das propriedades têm aspas duplas (" ")
+Arrays e objetos aninhados podem existir
+Restrição de trailing comma
+
+
+Além dessas diferenças que percebemos no trecho de exemplo, temos algumas outras:
+
+Funções não são permitidas, precisamos omiti-las do nosso objeto;
+Comentários não são permitidos;
+Suporta apenas tipos primitivos (string, number, boolean, null);
+O undefined também não é permitido;
+Algumas estruturas mais complexas, como datas ou a escrita em Regex, também podem não ser aceitas.
+*/
