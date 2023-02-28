995d1a891689f996d24a5ef549f2c1b267f6607d
Maria Eduarda <eduardaloopes2011@hotmail.com>
2023-02-28 11:52:56 -0300

    leitura-json

--- JSON/leitura-json.js
+++ JSON/leitura-json.js
@@ -1,6 +1,5 @@
 const dados = require("./cliente.json");
 // Require é uma função que o nosso ambiente node nos fornece para puxar o código de algum outro lugar
 // Nesse momento ele está puxando um arquivo JSON e transformando num objeto usável
-
 console.log(dados);
 console.log(typeof dados);
