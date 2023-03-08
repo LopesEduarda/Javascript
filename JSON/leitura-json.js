8bbc27f3345ff0b865a5f011e95b25d66e7af450
Maria Eduarda <eduardaloopes2011@hotmail.com>
2023-03-08 10:08:45 -0300

    stringify and parse

--- JSON/leitura-json.js
+++ JSON/leitura-json.js
@@ -3,3 +3,19 @@ const dados = require("./cliente.json");
 // Nesse momento ele está puxando um arquivo JSON e transformando num objeto usável
 console.log(dados);
 console.log(typeof dados);
+
+const clienteEmString = JSON.stringify(dados);
+console.log(clienteEmString);
+// transformar JSON em string 
+
+console.log(typeof clienteEmString);
+// string
+
+// é mais simples usar textos na tecnologia do que objetos. por isso a importancia de transformar 
+// json em objetos
+
+
+const clienteEmObjeto = JSON.parse(clienteEmString);
+console.log(clienteEmObjeto);
+console.log(typeof clienteEmObjeto);
+// transformar string de volta em objetos
