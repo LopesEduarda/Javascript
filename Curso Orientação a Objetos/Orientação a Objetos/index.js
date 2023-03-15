39da78de6b3e2a8d940cc461fa8ebb0062c7e4e6
Maria Eduarda <eduardaloopes2011@hotmail.com>
2023-03-15 18:14:07 -0300

    organizing the folders

---
+++ Curso Orientação a Objetos/Orientação a Objetos/index.js
@@ -0,0 +1,14 @@
+/*
+Trabalharemos em um novo projeto, encomendado pelo banco Bytebank, no qual criaremos um sistema que permite o cadastro de clientes e contas-correntes, além das operações bancárias mais comuns. Em uma nova pasta, criaremos o arquivo index.js, que servirá de entrada para nosso sistema.
+*/
+
+const cliente1Nome = "Ricardo";
+const cliente1Cpf = 11122233309;
+const cliente1Agencia = 1001;
+const cliente1Saldo = 0;
+
+const cliente2Nome = "Alice";
+const cliente2Cpf = 88822233309;
+const cliente2Agencia = 1001;
+const cliente2Saldo = 0;
+
