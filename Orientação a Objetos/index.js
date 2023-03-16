168f140f8b141d166514544ea62f14dba4231f93
Maria Eduarda <eduardaloopes2011@hotmail.com>
2023-03-16 09:49:51 -0300

    start with js class

--- Orientação a Objetos/index.js
+++ Orientação a Objetos/index.js
@@ -2,16 +2,29 @@
 Trabalharemos em um novo projeto, encomendado pelo banco Bytebank, no qual criaremos um sistema que permite o cadastro de clientes e contas-correntes, além das operações bancárias mais comuns. Em uma nova pasta, criaremos o arquivo index.js, que servirá de entrada para nosso sistema.
 */
 
-const cliente1Nome = "Ricardo";
-const cliente1Cpf = 11122233309;
-const cliente1Agencia = 1001;
-const cliente1Saldo = 0;
+class Cliente {
+    nome;
+    cpf;
+    agencia;
+    saldo;
+    rg;
+}
 
-const cliente2Nome = "Alice";
-const cliente2Cpf = 88822233309;
-const cliente2Agencia = 1001;
-const cliente2Saldo = 0;
+const cliente1 = new Cliente();
+const cliente2 = new Cliente();
+// criando uma instância da minha classe Cliente, que é um 'molde'
 
-console.log(cliente1Nome);
-console.log(cliente2Nome);
+cliente1.nome = "Ricardo";
+cliente1.cpf = 11122233309;
+cliente1.agencia = 1001;
+cliente1.saldo = 0;
+cliente1.rg = 123456789;
 
+cliente2.nome = "Alice";
+cliente2.cpf = 88822233309;
+cliente2.agencia = 1001;
+cliente2.saldo = 0;
+cliente2.rg = 789456123;
+
+console.log(cliente1);
+console.log(cliente2);
