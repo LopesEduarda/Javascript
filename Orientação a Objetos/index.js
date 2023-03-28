065671ab632975cbb08cf9b0a1151dec7916a7c6
Maria Eduarda <eduardaloopes2011@hotmail.com>
2023-03-28 14:14:44 -0300

    add new folder

--- Orientação a Objetos/index.js
+++ Orientação a Objetos/index.js
@@ -5,25 +5,39 @@ Trabalharemos em um novo projeto, encomendado pelo banco Bytebank, no qual criar
 class Cliente {
     nome;
     cpf;
+}
+
+class ContaCorrente {
     agencia;
     saldo;
-    rg;
 }
-const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-// criando uma instância da minha classe Cliente, que é um 'molde'
 
+const cliente1 = new Cliente();
+// criando uma instância da minha classe Cliente, que é um 'molde'
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
-cliente1.rg = 123456789;
 
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
-cliente2.rg = 789456123;
+
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.saldo = 0;
+contaCorrenteRicardo.agencia = 1001;
+
+console.log('Saldo:', contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.saldo = 100;
+console.log('Saldo atualizado:', contaCorrenteRicardo.saldo);
+let valorSacado = 200;
+    if (contaCorrenteRicardo.saldo >= valorSacado) {
+        contaCorrenteRicardo.saldo -= valorSacado;
+    } else {
+        console.log('Não possui saldo suficiente!')
+    }
+console.log('Saldo após valor sacado:', contaCorrenteRicardo.saldo);
+
+
+
 
 console.log(cliente1);
 console.log(cliente2);
