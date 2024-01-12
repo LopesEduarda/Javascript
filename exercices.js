bd345806105d87735e486aed1ab49c81a22153e7
Maria Eduarda <eduardaloopes2011@hotmail.com>
2024-01-12 16:09:58 -0300

    adding the statements

--- exercices.js
+++ exercices.js
@@ -1,1 +1,19 @@
 // exercícios pra treinar
+
+/*
+given a string
+reverse it
+example: reverse('apple')==='elppa'
+*/
+
+/*
+given a string - convert it into array
+then reverse it the array
+then reverse it back into string
+*/
+
+/*
+given string - create empty string
+loop through each character
+return the reverse string
+*/
